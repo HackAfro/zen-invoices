9db1ff1d68bb32f825595bdf0932a8da2e00806b
Richard Afro <richyafro@hotmail.com>
2020-02-16 23:07:53 +0100

    Main app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Table from './components/table';
+
+import AppProvider from './providers';
+import InvoiceCreateModal from './components/modal';
+import Header from './components/header';
+
 import './App.css';
+import { AppContext } from './contexts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <div className='mx-auto w-full py-4 px-4 md:w-4/5 md:py-8 md:px-6'>
+        <Header />
+        <div className='max-w-full overflow-x-scroll mt-8 md:mt-10'>
+          <Table />
+          <AppContext.Consumer>
+            {({ modalOpen }) => {
+              return modalOpen && <InvoiceCreateModal />;
+            }}
+          </AppContext.Consumer>
+        </div>
+      </div>
+    </AppProvider>
   );
 }
 
