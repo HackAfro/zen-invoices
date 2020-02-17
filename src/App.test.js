33c3217405714f975021bb594cde905bc58940cd
Richard Afro <richyafro@hotmail.com>
2020-02-17 06:29:28 +0100

    remove comments

--- src/App.test.js
+++ src/App.test.js
@@ -2,24 +2,6 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-// jest.mock('react', () => {
-//   const ActualReact = require.requireActual('react');
-//   return {
-//     ...ActualReact,
-//     useContext: () => ({
-//       invoices: [
-//         {
-//           id: 33,
-//           date: '2018-01-11',
-//           subject: 'Rent January',
-//           amount: '2200',
-//           iban: null
-//         }
-//       ],
-//     }) // what you want to return when useContext get fired goes here
-//   };
-// });
-
 test('shows invoice creation modal', () => {
   const { getByText } = render(<App />);
 
@@ -35,21 +17,3 @@ test('shows invoice creation modal', () => {
 
   expect(getByText(/Create Invoice/i)).toBeInTheDocument();
 });
-
-// test('edit click pre-fills the form', () => {
-//   const { getByTitle, getByDisplayValue } = render(<App />);
-
-//   const editButton = getByTitle(/Edit Invoice/i);
-
-//   fireEvent(
-//     editButton,
-//     new MouseEvent('click', {
-//       bubbles: true,
-//       cancelable: true
-//     })
-//   );
-
-//   expect(getByDisplayValue(/Rent January/i)).toBeInTheDocument();
-//   expect(getByDisplayValue(/2018-01-11/i)).toBeInTheDocument();
-//   expect(getByDisplayValue(/2200/i)).toBeInTheDocument();
-// });
