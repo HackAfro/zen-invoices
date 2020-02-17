3afedd16fd3f05cbaa2db07f29639346d34ae994
Richard Afro <richyafro@hotmail.com>
2020-02-17 05:15:30 +0100

    Entry test file for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,55 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// jest.mock('react', () => {
+//   const ActualReact = require.requireActual('react');
+//   return {
+//     ...ActualReact,
+//     useContext: () => ({
+//       invoices: [
+//         {
+//           id: 33,
+//           date: '2018-01-11',
+//           subject: 'Rent January',
+//           amount: '2200',
+//           iban: null
+//         }
+//       ],
+//     }) // what you want to return when useContext get fired goes here
+//   };
+// });
+
+test('shows invoice creation modal', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const newButton = getByText(/Add Invoice/i);
+
+  fireEvent(
+    newButton,
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    })
+  );
+
+  expect(getByText(/Create Invoice/i)).toBeInTheDocument();
 });
+
+// test('edit click pre-fills the form', () => {
+//   const { getByTitle, getByDisplayValue } = render(<App />);
+
+//   const editButton = getByTitle(/Edit Invoice/i);
+
+//   fireEvent(
+//     editButton,
+//     new MouseEvent('click', {
+//       bubbles: true,
+//       cancelable: true
+//     })
+//   );
+
+//   expect(getByDisplayValue(/Rent January/i)).toBeInTheDocument();
+//   expect(getByDisplayValue(/2018-01-11/i)).toBeInTheDocument();
+//   expect(getByDisplayValue(/2200/i)).toBeInTheDocument();
+// });
