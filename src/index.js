e5066d6cebfd849269da0a6a79e8fdf0867d7238
Richard Afro <richyafro@hotmail.com>
2020-02-17 05:16:00 +0100

    set app element for react-modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+ReactModal.setAppElement('#root');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
