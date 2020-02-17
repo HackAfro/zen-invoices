ffdadf46b8a97cb932b99ad26dcff8c913e60010
Richard Afro <richyafro@hotmail.com>
2020-02-17 05:26:03 +0100

    data file

---
+++ src/providers/data.js
@@ -0,0 +1,51 @@
+export const invoices = [
+  {
+    id: 33,
+    date: '2018-01-11',
+    subject: 'Rent January',
+    amount: '2200',
+    iban: null
+  },
+  {
+    id: 13,
+    date: '2018-02-23',
+    subject: 'Rent February',
+    amount: '400',
+    iban: 'DE-01-0000-2233-33222-12'
+  },
+  {
+    id: 23,
+    date: '2018-03-03',
+    subject: 'Rent March',
+    amount: '200',
+    iban: null
+  },
+  {
+    id: 3,
+    date: '2018-04-30',
+    subject: 'Rent April',
+    amount: '2200',
+    iban: 'DE-01-0000-2233-33222-41'
+  }
+];
+
+export const transfers = [
+  {
+    id: 53,
+    subject: 'Rent January',
+    amount: '230',
+    iban: 'DE-01-0000-2233-33222-12'
+  },
+  {
+    id: 50,
+    subject: 'Rent June',
+    amount: '110',
+    iban: 'DE-01-0000-2233-33242-10'
+  },
+  {
+    id: 5,
+    subject: 'Rent April',
+    amount: '2200',
+    iban: 'DE-01-0000-2233-13222-41'
+  }
+];
